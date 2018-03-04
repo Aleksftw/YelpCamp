e97dab649f95d53583ce60c732c1e5bf8e4590ff
Aleks <a.donev.11@aberdeen.ac.uk>
2018-03-04 14:13:19 +0000

    connect to mongoLAB

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v10");
+// mongoose.connect("mongodb://localhost/yelp_camp_v10");
+mongoose.connect("mongodb://Aleks:pass@ds255958.mlab.com:55958/project123");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
