5a4f69ae178574586ca45c1f20af0e9db93f91ad
Aleks <a.donev.11@aberdeen.ac.uk>
2018-03-04 14:37:10 +0000

    update databaseurl

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ var express     = require("express"),
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
-    
-// mongoose.connect("mongodb://localhost/yelp_camp_v10");
-mongoose.connect("mongodb://Aleks:pass@ds255958.mlab.com:55958/project123");
+ 
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://Aleks:pass@ds255958.mlab.com:55958/project123");
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
